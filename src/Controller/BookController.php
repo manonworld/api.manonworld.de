1346f9aafcd75fdf0cf9cbe9b7501083430ff6d1
mosta <info@manonworld.de>
2021-08-13 14:58:16 +0200

    Completed Find, Update and Delete. Closes #32

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,19 +11,31 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Exception\ValidationException;
 use App\Repository\BookRepository;
 use App\Service\BookCreator;
+use App\Service\BookDeleter;
+use App\Service\BookUpdater;
+use App\Entity\Book;
 
 class BookController extends AbstractController
 {
 
     private BookRepository $repo;
     private BookCreator $creator;
+    private BookDeleter $deleter;
+    private BookUpdater $updater;
+    private SerializerInterface $serializer;
 
     public function __construct(
         BookRepository $repo, 
-        BookCreator $creator
+        BookCreator $creator,
+        BookDeleter $deleter,
+        BookUpdater $updater,
+        SerializerInterface $serializer
     ){
-        $this->repo     = $repo;
-        $this->creator  = $creator;
+        $this->repo         = $repo;
+        $this->creator      = $creator;
+        $this->deleter      = $deleter;
+        $this->updater      = $updater;
+        $this->serializer   = $serializer;
     }
 
     /**
@@ -32,7 +45,7 @@ class BookController extends AbstractController
     {
         $data = [
             'status'    => 'OK',
-            'data'      => $this->repo->findAll()
+            'data'      => $this->repo->findBy(['is_deleted' => false])
         ];
 
         return $this->json($data);
@@ -51,13 +64,57 @@ class BookController extends AbstractController
         } catch (ValidationException $e) {
             return $this->json($e->getViolations(), $e->getCode());
         } catch (\Exception $e) {
-            print_r($e->getFile() . $e->getLine() . $e->getMessage());
-            exit;
             return $this->json(['error' => 'SERVER_ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
 
         return JsonResponse::fromJsonString($result, Response::HTTP_CREATED);
     }
-    
 
+    /**
+     * @Route("/books/{id}", name="get_book", methods={"GET"})
+     */
+    public function find(Book $book)
+    {
+        if( $book->getIsDeleted() === true ) {
+            $data = ['status' => 'error', 'message' => 'GONE'];
+            return new JsonResponse($data, Response::HTTP_GONE);
+        }
+
+        return JsonResponse::fromJsonString(
+            $this->serializer->serialize($book, 'json')
+        );
+    }
+
+    /**
+     * @Route("/books/{id}", name="delete_book", methods={"DELETE"})
+     */
+    public function delete(Book $book)
+    {
+        $this->deleter->delete($book);
+
+        $data = ['status' => 'OK', 'message' => 'DELETED'];
+
+        return new JsonResponse($data, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Route("/books/{id}", name="update_book", methods={"PUT"})
+     */
+    public function update(Book $book, Request $request)
+    {
+        if ( $book->getIsDeleted() === true ) {
+            $data = ['status' => 'error', 'message' => 'GONE'];
+            return new JsonResponse($data, Response::HTTP_GONE);
+        }
+        
+        try {
+            $result = $this->updater->update($book, $request->getContent());
+        } catch (ValidationException $e) {
+            return $this->json($e->getViolations(), $e->getCode());
+        } catch (\Exception $e) {
+            return $this->json(['error' => 'SERVER_ERROR'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        return JsonResponse::fromJsonString($result, Response::HTTP_ACCEPTED);
+    }
 }
---
+++ src/Service/BookUpdater.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Exception\ValidationException;
+use App\Repository\BookRepository;
+use App\Entity\Book;
+
+class BookUpdater
+{
+    private BookRepository $repo;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+
+    public function __construct(
+        BookRepository $repo, 
+        SerializerInterface $serializer, 
+        ValidatorInterface $validator
+    ){
+        $this->repo         = $repo;
+        $this->serializer   = $serializer;
+        $this->validator    = $validator;
+    }
+
+    public function update(Book $book, string $data)
+    {
+        $content = json_decode($data);
+
+        if ( isset( $content->isbn ) )
+            $book->setIsbn( $content->isbn );
+
+        if ( isset( $content->title ) )
+            $book->setTitle( $content->title );
+
+        if ( isset( $content->description ) )
+            $book->setDescription( $content->description );
+
+        if ( isset( $content->url ) )
+            $book->setUrl( $content->url );
+
+        $errors = $this->validator->validate($book);
+
+        if ( count( $errors ) )
+            throw (new ValidationException)->setViolations($errors);
+
+        $updatedBook = $this->repo->save($book);
+
+        return $this->serializer->serialize($updatedBook, 'json');
+    }
+}
